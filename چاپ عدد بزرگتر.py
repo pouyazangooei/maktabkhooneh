a14fe712b33ffdb222fd7e718175607722fbacb5
pouyazangooei <134936780+pouyazangooei@users.noreply.github.com>
2023-05-29 15:48:18 +0330

    Add files via upload
    در این برنامه دو عدد از ورودی می خوانید و عدد بزرگتر را در خروجی چاپ می کنید. خط اول ورودی شامل عدد اول است. خط دوم ورودی شامل عدد دوم است. تضمین می شود اعداد ورودی اعداد صحیح مثبت هستند. در صورت مساوی بودن دو عدد ورودی فرقی نمی کند، یکی از آنها را چاپ کنید.

---
+++ چاپ عدد بزرگتر.py
@@ -0,0 +1,11 @@
+number1 = int(input())
+number2 = int(input())
+if number1 > 0 and number2 > 0:
+    if number1 > number2:
+        print(number1)
+    elif number2 > number1:
+        print(number2)
+    else:
+        print(number1)
+else:
+    print('mosbat nist amooyi!')
